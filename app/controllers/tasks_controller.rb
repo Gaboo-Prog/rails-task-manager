ee6e48eac61ac46da68036709ef567d41833e443
gaboo <sgaboo@yahoo.com>
2021-08-05 13:07:49 -0400

    Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,28 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+      if @task.save
+        redirect_to task_path(@task)
+      else
+        render :new
+    end
+    
+    def update
+    
+    end
+  end 
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
